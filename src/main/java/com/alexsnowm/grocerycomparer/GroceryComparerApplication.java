d490281aa284d7473009921a17cf4a96dca0cee6
Alex Min <11alexmin@gmail.com>
2018-02-01 02:02:35 -0600

    Committed unversioned files of grocery-comparer-named project for first time

---
+++ src/main/java/com/alexsnowm/grocerycomparer/GroceryComparerApplication.java
@@ -0,0 +1,12 @@
+package com.alexsnowm.grocerycomparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryComparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryComparerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexsnowm/grocerycomparer/GroceryComparerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexsnowm.grocerycomparer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroceryComparerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
